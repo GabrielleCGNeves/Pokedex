fe608f11cc1b60ce3656dd1fb9c4317734669b76
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2023-02-24 18:10:50 -0300

    :sparkles: big bang

---
+++ Pokedex/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Pokedex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCharmander = new System.Windows.Forms.Button();
+            this.btnSquirtle = new System.Windows.Forms.Button();
+            this.btnBulbasaur = new System.Windows.Forms.Button();
+            this.btnWeedle = new System.Windows.Forms.Button();
+            this.btnCaterpie = new System.Windows.Forms.Button();
+            this.btnEkans = new System.Windows.Forms.Button();
+            this.btnSandshrew = new System.Windows.Forms.Button();
+            this.btnJigglypuff = new System.Windows.Forms.Button();
+            this.btnPidgey = new System.Windows.Forms.Button();
+            this.btnRattata = new System.Windows.Forms.Button();
+            this.pictureBoxPokedex = new System.Windows.Forms.PictureBox();
+            this.pictureBoxBackgound = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPokedex)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBackgound)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCharmander
+            // 
+            this.btnCharmander.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
+            this.btnCharmander.Font = new System.Drawing.Font("Century Schoolbook", 8.5F, System.Drawing.FontStyle.Bold);
+            this.btnCharmander.Location = new System.Drawing.Point(272, 55);
+            this.btnCharmander.Name = "btnCharmander";
+            this.btnCharmander.Size = new System.Drawing.Size(121, 26);
+            this.btnCharmander.TabIndex = 1;
+            this.btnCharmander.Text = "Charmander";
+            this.btnCharmander.UseVisualStyleBackColor = false;
+            this.btnCharmander.Click += new System.EventHandler(this.btnCharmander_Click);
+            // 
+            // btnSquirtle
+            // 
+            this.btnSquirtle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
+            this.btnSquirtle.Font = new System.Drawing.Font("Century Schoolbook", 8.5F, System.Drawing.FontStyle.Bold);
+            this.btnSquirtle.Location = new System.Drawing.Point(272, 87);
+            this.btnSquirtle.Name = "btnSquirtle";
+            this.btnSquirtle.Size = new System.Drawing.Size(86, 26);
+            this.btnSquirtle.TabIndex = 2;
+            this.btnSquirtle.Text = "Squirtle";
+            this.btnSquirtle.UseVisualStyleBackColor = false;
+            this.btnSquirtle.Click += new System.EventHandler(this.btnSquirtle_Click);
+            // 
+            // btnBulbasaur
+            // 
+            this.btnBulbasaur.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
+            this.btnBulbasaur.Font = new System.Drawing.Font("Century Schoolbook", 8.5F, System.Drawing.FontStyle.Bold);
+            this.btnBulbasaur.Location = new System.Drawing.Point(272, 118);
+            this.btnBulbasaur.Name = "btnBulbasaur";
+            this.btnBulbasaur.Size = new System.Drawing.Size(100, 26);
+            this.btnBulbasaur.TabIndex = 3;
+            this.btnBulbasaur.Text = "Bulbasaur";
+            this.btnBulbasaur.UseVisualStyleBackColor = false;
+            this.btnBulbasaur.Click += new System.EventHandler(this.btnBulbasaur_Click);
+            // 
+            // btnWeedle
+            // 
+            this.btnWeedle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
+            this.btnWeedle.Font = new System.Drawing.Font("Century Schoolbook", 8.5F, System.Drawing.FontStyle.Bold);
+            this.btnWeedle.Location = new System.Drawing.Point(272, 184);
+            this.btnWeedle.Name = "btnWeedle";
+            this.btnWeedle.Size = new System.Drawing.Size(79, 26);
+            this.btnWeedle.TabIndex = 4;
+            this.btnWeedle.Text = "Weedle";
+            this.btnWeedle.UseVisualStyleBackColor = false;
+            this.btnWeedle.Click += new System.EventHandler(this.btnWeedle_Click);
+            // 
+            // btnCaterpie
+            // 
+            this.btnCaterpie.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
+            this.btnCaterpie.Font = new System.Drawing.Font("Century Schoolbook", 8.5F, System.Drawing.FontStyle.Bold);
+            this.btnCaterpie.Location = new System.Drawing.Point(272, 153);
+            this.btnCaterpie.Name = "btnCaterpie";
+            this.btnCaterpie.Size = new System.Drawing.Size(88, 26);
+            this.btnCaterpie.TabIndex = 5;
+            this.btnCaterpie.Text = "Caterpie";
+            this.btnCaterpie.UseVisualStyleBackColor = false;
+            this.btnCaterpie.Click += new System.EventHandler(this.btnCaterpie_Click);
+            // 
+            // btnEkans
+            // 
+            this.btnEkans.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
+            this.btnEkans.Font = new System.Drawing.Font("Century Schoolbook", 8.5F, System.Drawing.FontStyle.Bold);
+            this.btnEkans.Location = new System.Drawing.Point(272, 281);
+            this.btnEkans.Name = "btnEkans";
+            this.btnEkans.Size = new System.Drawing.Size(72, 26);
+            this.btnEkans.TabIndex = 6;
+            this.btnEkans.Text = "Ekans";
+            this.btnEkans.UseVisualStyleBackColor = false;
+            this.btnEkans.Click += new System.EventHandler(this.btnEkans_Click);
+            // 
+            // btnSandshrew
+            // 
+            this.btnSandshrew.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
+            this.btnSandshrew.Font = new System.Drawing.Font("Century Schoolbook", 8.5F, System.Drawing.FontStyle.Bold);
+            this.btnSandshrew.Location = new System.Drawing.Point(273, 311);
+            this.btnSandshrew.Name = "btnSandshrew";
+            this.btnSandshrew.Size = new System.Drawing.Size(105, 26);
+            this.btnSandshrew.TabIndex = 7;
+            this.btnSandshrew.Text = "Sandshrew";
+            this.btnSandshrew.UseVisualStyleBackColor = false;
+            this.btnSandshrew.Click += new System.EventHandler(this.btnSandshrew_Click);
+            // 
+            // btnJigglypuff
+            // 
+            this.btnJigglypuff.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
+            this.btnJigglypuff.Font = new System.Drawing.Font("Century Schoolbook", 8.5F, System.Drawing.FontStyle.Bold);
+            this.btnJigglypuff.Location = new System.Drawing.Point(273, 343);
+            this.btnJigglypuff.Name = "btnJigglypuff";
+            this.btnJigglypuff.Size = new System.Drawing.Size(102, 26);
+            this.btnJigglypuff.TabIndex = 8;
+            this.btnJigglypuff.Text = "Jigglypuff ";
+            this.btnJigglypuff.UseVisualStyleBackColor = false;
+            this.btnJigglypuff.Click += new System.EventHandler(this.btnJigglypuff_Click);
+            // 
+            // btnPidgey
+            // 
+            this.btnPidgey.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
+            this.btnPidgey.Font = new System.Drawing.Font("Century Schoolbook", 8.5F, System.Drawing.FontStyle.Bold);
+            this.btnPidgey.Location = new System.Drawing.Point(273, 215);
+            this.btnPidgey.Name = "btnPidgey";
+            this.btnPidgey.Size = new System.Drawing.Size(72, 26);
+            this.btnPidgey.TabIndex = 9;
+            this.btnPidgey.Text = "Pidgey";
+            this.btnPidgey.UseVisualStyleBackColor = false;
+            this.btnPidgey.Click += new System.EventHandler(this.btnPidgey_Click);
+            // 
+            // btnRattata
+            // 
+            this.btnRattata.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
+            this.btnRattata.Font = new System.Drawing.Font("Century Schoolbook", 8.5F, System.Drawing.FontStyle.Bold);
+            this.btnRattata.Location = new System.Drawing.Point(272, 248);
+            this.btnRattata.Name = "btnRattata";
+            this.btnRattata.Size = new System.Drawing.Size(75, 26);
+            this.btnRattata.TabIndex = 10;
+            this.btnRattata.Text = "Rattata";
+            this.btnRattata.UseVisualStyleBackColor = false;
+            this.btnRattata.Click += new System.EventHandler(this.btnRattata_Click);
+            // 
+            // pictureBoxPokedex
+            // 
+            this.pictureBoxPokedex.Image = global::Pokedex.Properties.Resources.gif_charmander;
+            this.pictureBoxPokedex.Location = new System.Drawing.Point(77, 160);
+            this.pictureBoxPokedex.Name = "pictureBoxPokedex";
+            this.pictureBoxPokedex.Size = new System.Drawing.Size(65, 67);
+            this.pictureBoxPokedex.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxPokedex.TabIndex = 11;
+            this.pictureBoxPokedex.TabStop = false;
+            // 
+            // pictureBoxBackgound
+            // 
+            this.pictureBoxBackgound.Image = global::Pokedex.Properties.Resources.pokedex;
+            this.pictureBoxBackgound.Location = new System.Drawing.Point(1, 0);
+            this.pictureBoxBackgound.Name = "pictureBoxBackgound";
+            this.pictureBoxBackgound.Size = new System.Drawing.Size(512, 386);
+            this.pictureBoxBackgound.TabIndex = 0;
+            this.pictureBoxBackgound.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 384);
+            this.Controls.Add(this.pictureBoxPokedex);
+            this.Controls.Add(this.btnRattata);
+            this.Controls.Add(this.btnPidgey);
+            this.Controls.Add(this.btnJigglypuff);
+            this.Controls.Add(this.btnSandshrew);
+            this.Controls.Add(this.btnEkans);
+            this.Controls.Add(this.btnCaterpie);
+            this.Controls.Add(this.btnWeedle);
+            this.Controls.Add(this.btnBulbasaur);
+            this.Controls.Add(this.btnSquirtle);
+            this.Controls.Add(this.btnCharmander);
+            this.Controls.Add(this.pictureBoxBackgound);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPokedex)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBackgound)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxBackgound;
+        private System.Windows.Forms.Button btnCharmander;
+        private System.Windows.Forms.Button btnSquirtle;
+        private System.Windows.Forms.Button btnBulbasaur;
+        private System.Windows.Forms.Button btnWeedle;
+        private System.Windows.Forms.Button btnCaterpie;
+        private System.Windows.Forms.Button btnEkans;
+        private System.Windows.Forms.Button btnSandshrew;
+        private System.Windows.Forms.Button btnJigglypuff;
+        private System.Windows.Forms.Button btnPidgey;
+        private System.Windows.Forms.Button btnRattata;
+        private System.Windows.Forms.PictureBox pictureBoxPokedex;
+    }
+}
+
---
+++ Pokedex/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pokedex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCharmander_Click(object sender, EventArgs e)
+        {
+            pictureBoxPokedex.Image = Properties.Resources.gif_charmander;
+        }
+
+        private void btnSquirtle_Click(object sender, EventArgs e)
+        {
+            pictureBoxPokedex.Image = Properties.Resources.gif_squirtle;
+        }
+
+        private void btnBulbasaur_Click(object sender, EventArgs e)
+        {
+            pictureBoxPokedex.Image = Properties.Resources.gif_bulbasaur;
+        }
+
+        private void btnCaterpie_Click(object sender, EventArgs e)
+        {
+            pictureBoxPokedex.Image = Properties.Resources.gif_caterpie;
+        }
+
+        private void btnWeedle_Click(object sender, EventArgs e)
+        {
+            pictureBoxPokedex.Image = Properties.Resources.gif_weedle;
+        }
+
+        private void btnPidgey_Click(object sender, EventArgs e)
+        {
+            pictureBoxPokedex.Image = Properties.Resources.gif_pidgey;
+        }
+
+        private void btnRattata_Click(object sender, EventArgs e)
+        {
+            pictureBoxPokedex.Image = Properties.Resources.gif_rattata;
+        }
+
+        private void btnEkans_Click(object sender, EventArgs e)
+        {
+            pictureBoxPokedex.Image = Properties.Resources.gif_ekans;
+        }
+
+        private void btnSandshrew_Click(object sender, EventArgs e)
+        {
+            pictureBoxPokedex.Image = Properties.Resources.gif_sandshrew;
+        }
+
+        private void btnJigglypuff_Click(object sender, EventArgs e)
+        {
+            pictureBoxPokedex.Image = Properties.Resources.gif_jigglypuff;
+        }
+    }
+}
